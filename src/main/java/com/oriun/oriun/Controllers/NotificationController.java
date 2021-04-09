0da1d7b62038852fc3c230fe381ee1149d7d2812
Oscar Andres Mancera Garzón <oamancerag@unal.edu.co>
2021-04-08 22:14:24 -0500

    Create NotificationController.java

---
+++ src/main/java/com/oriun/oriun/Controllers/NotificationController.java
@@ -0,0 +1,27 @@
+package com.oriun.oriun.Controllers;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import com.oriun.oriun.Models.NotificationModel;
+import com.oriun.oriun.Models.User_eventModel;
+import com.oriun.oriun.Services.NotificationService;
+
+@RestController
+//@RequestMapping("/notifications")
+public class NotificationController {
+    @Autowired
+    NotificationService notificationService;
+    
+    @GetMapping("/events")
+    public ArrayList<NotificationModel> obtenerNotificaciones(){
+        return notificationService.getNotifications();
+    }
+    
+    @PostMapping("/event")
+    public NotificationModel guardarnotificacion(@RequestBody NotificationModel notification){
+        return this.notificationService.saveNotification(notification);
+    }
+    
+}
