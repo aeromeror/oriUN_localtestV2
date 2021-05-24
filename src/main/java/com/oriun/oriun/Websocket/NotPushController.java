c6b715fb621d0d5ad5dfa3508239ca7480ae0a53
Oscar Andres Mancera Garzón <30636302+Alasienn@users.noreply.github.com>
2021-05-24 12:20:39 -0500

    Notif Test

--- src/main/java/com/oriun/oriun/Websocket/NotPushController.java
+++ src/main/java/com/oriun/oriun/Websocket/NotPushController.java
@@ -3,23 +3,26 @@ package com.oriun.oriun.Websocket;
 import java.util.List;
 
 import com.oriun.oriun.Models.NotificationModel;
+import com.oriun.oriun.Models.SportModel;
 import com.oriun.oriun.Services.NotificationService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.util.HtmlUtils;
 
 @Controller
 public class NotPushController {
   @Autowired
   NotificationService notificationService;
-  /*@SendTo("/topic/greetings")
-  public List<NotificationModel> greeting(String name) throws Exception {
-    Thread.sleep(1000); // simulated delay
-    
-    return notificationService.;
-  }*/
+  
+  @MessageMapping("/eventCreated")
+    @SendTo("/topic/notifications")
+    public List<NotificationModel> notify(SportModel NAME_SPORT) throws Exception {
+        List< NotificationModel> notifications = notificationService.getNotificationBySport(NAME_SPORT.getNAME_SPORT());
+        System.out.println(NAME_SPORT);
+        return notifications;
+
+    }
 
 }
