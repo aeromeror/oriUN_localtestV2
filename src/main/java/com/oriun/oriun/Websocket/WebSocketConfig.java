f1b6055bf1a86a584bc9756b511e067f65d2a548
aeromeror <aeromeror@unal.edu.co>
2021-05-25 17:32:42 -0500

    websocket change

--- src/main/java/com/oriun/oriun/Websocket/WebSocketConfig.java
+++ src/main/java/com/oriun/oriun/Websocket/WebSocketConfig.java
@@ -18,7 +18,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
-    registry.addEndpoint("/websocket").setAllowedOrigins("http://localhost:8080").withSockJS();
+    registry.addEndpoint("/websocket").setAllowedOrigins("https://oriun.herokuapp.com").withSockJS();
   }
 
 }
